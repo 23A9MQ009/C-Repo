b0f6c0a36aec80c4dda7c525a9a62b5c4d987982
Srinitya Palli <147475509+23A9MQ009@users.noreply.github.com>
2025-01-01 20:20:21 +0530

    Uploading code: Can Cross the Bridge.c

--- Can Cross the Bridge.c
+++ Can Cross the Bridge.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
-
 int main() {
-    int X, Y, Z;
-    
-    // Read the input values for mango weight (X), truck weight (Y), and bridge weight limit (Z)
+    int X, Y, Z, max_mangoes;
     scanf("%d %d %d", &X, &Y, &Z);
-    
-    // Calculate the maximum number of mangoes that can be loaded
-    int max_mangoes = (Z - Y) / X;
-    
-    // Print the maximum number of mangoes
+    max_mangoes = (Z - Y) / X;
     printf("%d\n", max_mangoes);
-    
     return 0;
 }
