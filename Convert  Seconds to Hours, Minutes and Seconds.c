129e87f9ec0ff897a213d396ed92b01ed2fe0437
Srinitya Palli <147475509+23A9MQ009@users.noreply.github.com>
2025-01-01 20:49:24 +0530

    Uploading code: Convert  Seconds to Hours, Minutes and Seconds.c

---
+++ Convert  Seconds to Hours, Minutes and Seconds.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int total_seconds;
+    int hours, minutes, seconds;
+    scanf("%d", &total_seconds);
+    hours = total_seconds / 3600;
+    total_seconds = total_seconds % 3600; 
+    minutes = total_seconds / 60; 
+    seconds = total_seconds % 60; 
+    printf("H:M:S-%d:%d:%d\n", hours, minutes, seconds);
+    return 0;
+}
