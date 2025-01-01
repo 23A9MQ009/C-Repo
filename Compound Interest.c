2a6e267b7e4a69a7cc56985da76756deb4c2011e
Srinitya Palli <147475509+23A9MQ009@users.noreply.github.com>
2025-01-01 20:26:56 +0530

    Uploading code: Compound Interest.c

---
+++ Compound Interest.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double P, R, T, CI;
+    scanf("%lf %lf %lf", &P, &R, &T);
+    CI = P * (pow(1 + R / 100, T) - 1);
+    printf("%.2f\n", CI);
+    return 0;
+}
